1415e8ed5db20dbc9d04ec7e833b5a7277b1be15
Mohammed Subhan Khan <khansubhan95@gmail.com>
2019-07-12 17:39:47 -0500

    Edit config

--- src/main/java/me/mskhan/blogsystem/BlogsystemApplication.java
+++ src/main/java/me/mskhan/blogsystem/BlogsystemApplication.java
@@ -2,9 +2,10 @@ package me.mskhan.blogsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogsystemApplication {
+public class BlogsystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogsystemApplication.class, args);
