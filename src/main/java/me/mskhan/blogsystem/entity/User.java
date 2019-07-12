fac7060528d2c95ee72745d0535430cd1eb2670e
Mohammed Subhan Khan <khansubhan95@gmail.com>
2019-07-12 17:19:21 -0500

    Remove dll

--- src/main/java/me/mskhan/blogsystem/entity/User.java
+++ src/main/java/me/mskhan/blogsystem/entity/User.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
